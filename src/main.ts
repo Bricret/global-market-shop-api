5811245c45340486de40bb344b8e7c65eea4631b
Brian Rico <114421216+Bricret@users.noreply.github.com>
2024-04-27 15:50:59 -0600

    Agregar cambios en el archivo main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
@@ -14,5 +15,6 @@ async function bootstrap() {
   app.setGlobalPrefix('api')
 
   await app.listen(process.env.PORT || 3000);
+  logger.log(`Application listening on port ${process.env.PORT}`);
 }
 bootstrap();
