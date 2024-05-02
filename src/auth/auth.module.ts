6b008942a01b103a31e8ef745bffbd8215f532dd
Brian Rico <114421216+Bricret@users.noreply.github.com>
2024-05-02 14:30:48 -0600

    Agregar configuración de JWT_SECRET en el  módulo de autenticación y actualizacion del template de variables de entorno

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,18 +1,39 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './entities/user.entity';
+
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 import { CommonModule } from 'src/common/common.module';
+import { User } from './entities/user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   controllers: [AuthController],
   providers: [AuthService],
   imports: [
-    TypeOrmModule.forFeature([
-      User
-    ]),
+
+    TypeOrmModule.forFeature([ User ]),
+
     CommonModule,
+
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+
+      useFactory: ( configService: ConfigService ) => {
+
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: '2h'
+          }
+        }
+      }
+    })
   ]
 })
 export class AuthModule {}
