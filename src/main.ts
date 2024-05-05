f95d41644208da5ec20a4b0f71b3eb1ecfe2aca1
Brian Rico <114421216+Bricret@users.noreply.github.com>
2024-05-05 15:06:25 -0600

    instalacion de swagger para documentacion de la API

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,27 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+
+import { AppModule } from './app.module';
 
 async function bootstrap() {
   const logger = new Logger('bootstrap');
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
     })
    );
+
+   const config = new DocumentBuilder()
+    .setTitle('Global Market API')
+    .setDescription('The Global Market API endPoints.')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   app.setGlobalPrefix('api')
 
